5979ddca26434556f6b9117a9dda671d334768be
lamp365 <791845283@qq.com>
2015-06-29 21:53:00 +0800

    配置文件复制和删除的shell脚本

---
+++ del.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+	rm -rf  $1wechat/Page/Conf/
+	rm -rf  $1wechat/Acenter/Conf/
+	rm -rf  $1wechat/Cli/Conf/
+	rm -rf  $1wechat/Common/Conf/
+	rm -rf  $1wechat/Script/Conf/
+	rm -rf  $1wechat/Server/Conf/
+	rm -rf  $1wechat/Ucenter/Conf/
+     	
+	echo "rm ok!"
+
