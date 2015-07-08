9613402db68fd69de24dd6aed005f2f57c69665c
xyzcms <501773740@qq.com>
2015-07-09 00:57:35 +0800

    demo 测试

---
+++ wechat/Server/Conf/Conf/email.php
@@ -0,0 +1,13 @@
+<?php 
+return array (
+  'email' => 'true',
+  'email_server' => 'stmp@qq.com',
+  'email_port' => '25',
+  'email_form' => 'kevin.liu@yunzhihui.com',
+  'email_user' => 'Kevin',
+  'email_pwd' => 'xxoo',
+  'reg_email_title' => '欢迎你的注册！',
+  'reg_email_content' => '有你更精彩！',
+  'pwd_email_title' => '密码找回',
+  'pwd_email_content' => '系统给你的随随机密码如下：12321',
+);
---
+++ wechat/Server/Conf/Conf/upfile.php
@@ -0,0 +1,6 @@
+<?php 
+return array (
+  'up_size' => '2048',
+  'up_exts' => 'jpeg,jpg,png',
+  'up_path' => './Public/upload',
+);
