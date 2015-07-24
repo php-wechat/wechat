e3c4c779f8e54a6eade6932f9870f480276ab621
aa791845283 <kevin.liu@yunzhihui.com>
2015-07-25 01:08:16 +0800

    处理function 被干掉的代码

--- wechat/Common/Common/function.php
+++ wechat/Common/Common/function.php
@@ -162,4 +162,66 @@ function think_decrypt($data, $key = '')
     return base64_decode($str);
 }
 
+
+
+/**
+ * @content socketLog函数
+ * @param $log
+ * @param string $type
+ * @param string $css
+ * @return mixed|void
+ * @throws Exception
+ */
+function slog($log,$type='log',$css='')
+{
+
+    if(is_string($type))
+    {
+        $socket = \Think\SocketLog::getInstance();
+
+        $type=preg_replace_callback('/_([a-zA-Z])/',create_function('$matches', 'return strtoupper($matches[1]);'),$type);
+        if(method_exists($socket,$type) || in_array($type,\Think\SocketLog::$log_types))
+        {
+            return call_user_func(array($socket,$type),$log,$css);
+        }
+    }
+
+    if(is_object($type) && 'mysqli'==get_class($type))
+    {
+        return \Think\SocketLog::mysqlilog($log,$type);
+    }
+
+    if(is_resource($type) && ('mysql link'==get_resource_type($type) || 'mysql link persistent'==get_resource_type($type)))
+    {
+        return \Think\SocketLog::mysqllog($log,$type);
+    }
+
+    if(is_object($type) && 'PDO'==get_class($type))
+    {
+        return \Think\SocketLog::pdolog($log,$type);
+    }
+
+    throw new Exception($type.' is not SocketLog method');
+}
+
+
+
+/**
+ * @content 转换字节数为其他单位
+ * @param $filesize
+ * @return string
+ */
+function sizecount($filesize) {
+    if ($filesize >= pow(1024,3)) {
+        $filesize = round($filesize / pow(1024,3), 2) .' GB';
+    } elseif ($filesize >= pow(1024,2)) {
+        $filesize = round($filesize / pow(1024,2), 2) .' MB';
+    } elseif($filesize >= pow(1024,1)) {
+        $filesize = round($filesize / pow(1024,1), 2) . ' KB';
+    } else {
+        $filesize = $filesize.' Bytes';
+    }
+    return $filesize;
+}
+
 ?>
