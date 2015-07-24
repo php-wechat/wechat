c8ddd92440ff660ba35eba9d4efe889817068d75
aa791845283 <kevin.liu@yunzhihui.com>
2015-07-25 02:46:27 +0800

    角色的页面

--- wechat/Server/Controller/GroupController.class.php
+++ wechat/Server/Controller/GroupController.class.php
@@ -1,11 +1,13 @@
 <?php
 namespace Server\Controller;
 use Think\Controller;
-class GroupController extends Controller {
+class GroupController extends BaseController {
 
    public function index()
    {
-	$this->display();
+       $data = M('root_role')->select();
+       $this->assign('data',$data);
+	  $this->display();
    }
 
     public function add()
@@ -13,13 +15,26 @@ class GroupController extends Controller {
         $this->display();
     }
 
+    public function doAdd()
+    {
+        $res = M('root_role')->add($_POST);
+        if($res)
+        {
+            $this->success('添加成功！',U('Group/index'));
+        }else{
+            $this->error('添加失败');
+        }
+    }
+
     public function edit()
     {
         $this->display();
     }
 
-    public function access()
+    //设置权限
+    public function roleSet()
     {
+        die('设置权限');
         $this->display();
     }
 }
